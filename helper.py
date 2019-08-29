0af32faeb56f1287cff7b03bfe8d42d2a15ff112
AsmaBalamash <asoomah1990@yahoo.com>
2019-08-29 20:50:52 +0300

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+
+
+def get_description(column_name, schema):
+    '''
+    INPUT - schema - pandas dataframe with the schema of the developers survey
+            column_name - string - the name of the column you would like to know about
+    OUTPUT - 
+            desc - string - the description of the column
+    '''
+    desc = list(schema[schema['Column'] == column_name]['Question'])[0]
+    return desc
+
+
+def total_count(df, col1, col2, look_for):
+    '''
+    INPUT:
+    df - the pandas dataframe you want to search
+    col1 - the column name you want to look through
+    col2 - the column you want to count values from
+    look_for - a list of strings you want to search for in each row of df[col]
+
+    OUTPUT:
+    new_df - a dataframe of each look_for with the count of how often it shows up
+    '''
+    new_df = defaultdict(int)
+    #loop through list of ed types
+    for val in look_for:
+        #loop through rows
+        for idx in range(df.shape[0]):
+            #if the ed type is in the row add 1
+            if val in df[col1][idx]:
+                new_df[val] += int(df[col2][idx])
+    new_df = pd.DataFrame(pd.Series(new_df)).reset_index()
+    new_df.columns = [col1, col2]
+    new_df.sort_values('count', ascending=False, inplace=True)
+    return new_df
